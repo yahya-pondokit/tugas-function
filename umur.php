f4e339705d2ac28248411730b7e6e59543d20fc8
yahya-pondokit <31057060+yahya-pondokit@users.noreply.github.com>
2017-12-02 20:57:02 +0700

    Update umur.php

--- umur.php
+++ umur.php
@@ -1,8 +1,31 @@
 <?php
-	function umur($umur = '1998'){
-		$time = date('Y');
-		echo $time - $umur;
+	function umur($umur = '1996-12'){
+		$tgl = date('Y-m');
+		$dikurangi = strtotime($tgl);
+		$pengurang = strtotime($umur);
+		$hasilnya = $dikurangi - $pengurang;
+		$hasilakhir = date('Y-m', $hasilnya);
+		$taun = substr($hasilakhir, 0, 4);
+		$taunudahdikurang = $taun - 1970;
+		$bulan = substr($hasilakhir, -2);
+		$bulanudahdikurang = $bulan - 1;
+		// echo $dikurangi;
+		// echo "<br>";
+		// echo $pengurang;
+		// echo "<br>";
+		// echo $hasilnya;
+		// echo "<br>";
+		// echo $hasilakhir;
+		// echo "<br>";
+		// echo $taun;
+		// echo "<br>";
+		// echo $taunudahdikurang;
+		// echo "<br>";
+		// echo $bulan;
+		// echo "<br>";
+		// echo $bulanudahdikurang;
+		// echo "<br>";
+		echo "Umur anda adalah ".$taunudahdikurang." tahun, ".$bulanudahdikurang." bulan.";
 	}
-	umur('1996');
+	umur();
 ?>
-		
