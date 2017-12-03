fe5644a050b8a4565e3721023abddb5b1f36e7e6
yahya-pondokit <31057060+yahya-pondokit@users.noreply.github.com>
2017-12-03 07:25:03 +0700

    Update umur.php

--- umur.php
+++ umur.php
@@ -1,31 +1,36 @@
+<DOCTYPE html>
+<html lang="id">
+	<head>
+	</head>
+	<body>
+		<form method="post">
+			Masukkan tanggal lahir anda dibawah:<br>
+			<input type="text" name="umur" placeholder="19xx-12-21" required>
+			<input type="submit" value="Hitung umur saya">
+		</form>
 <?php
-	function umur($umur = '1991-07'){
-		$tgl = date('Y-m');
+	function umur($umur = '1991-12-23'){
+		if (strlen($umur)==10 && strlen(trim($umur,'1234567890-')) == 0 && (substr($umur,4,1) && substr($umur,-3,1) == '-')){
+		$tgl = date('Y-m-d');
 		$dikurangi = strtotime($tgl);
 		$pengurang = strtotime($umur);
 		$hasilnya = $dikurangi - $pengurang;
-		$hasilakhir = date('Y-m', $hasilnya);
+		$hasilakhir = date('Y-m-d', $hasilnya);
 		$taun = substr($hasilakhir, 0, 4);
 		$taunudahdikurang = $taun - 1970;
-		$bulan = substr($hasilakhir, -2);
+		$bulan = substr($hasilakhir, 5, 2);
 		$bulanudahdikurang = $bulan - 1;
-		// echo $dikurangi;
-		// echo "<br>";
-		// echo $pengurang;
-		// echo "<br>";
-		// echo $hasilnya;
-		// echo "<br>";
-		// echo $hasilakhir;
-		// echo "<br>";
-		// echo $taun;
-		// echo "<br>";
-		// echo $taunudahdikurang;
-		// echo "<br>";
-		// echo $bulan;
-		// echo "<br>";
-		// echo $bulanudahdikurang;
-		// echo "<br>";
-		echo "Umur anda adalah ".$taunudahdikurang." tahun, ".$bulanudahdikurang." bulan.";
+		$hari = substr($hasilakhir, -2);
+		$hariudahdikurang = $hari - 1;
+		echo "Umur anda adalah ".$taunudahdikurang." tahun, ".$bulanudahdikurang." bulan dan ".$hariudahdikurang." hari.";
+		}else{
+			echo "Silahkan isi form dengan benar :)";
+		}
 	}
-	umur();
+	if (!empty($_POST['umur']) && isset($_POST['umur'])){
+	umur($_POST['umur']);
+	}
 ?>
+	</body>
+</html>
+		
