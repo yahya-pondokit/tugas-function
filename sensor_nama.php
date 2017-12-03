20c65796e49bde7e8f97e97e7296a4a7b25a6d4f
yahya-pondokit <31057060+yahya-pondokit@users.noreply.github.com>
2017-12-03 07:24:25 +0700

    Update sensor_nama.php

--- sensor_nama.php
+++ sensor_nama.php
@@ -1,11 +1,28 @@
+<DOCTYPE html>
+<html lang="id">
+	<head>
+	</head>
+	<body>
+		<form method="post">
+			Masukkan nama anda dibawah:<br>
+			<input type="text" name="nama" placeholder="isi disini" required>
+			<input type="submit" value="Sensor">
+		</form>
 <?php
-	function sensor($nama="Siswanto"){
-		$p = strlen($nama);
-		$l = substr($nama, 0, $p/2);
-		echo $l;
-		for ($i = 0; $i < $p/2;$i++){
-			echo "*";
+	function sensor($nama){
+		if ($nama == 'Yahya' OR $nama == 'Bilal' OR $nama == 'Farid'){
+			$l = substr($nama, 0, 2);
+			echo $l;
+			for ($i = 0; $i < 3;$i++){
+				echo "*";
+			}
+		}else{
+			echo "Nama ".$nama." tidak disensor ᶘ ᵒᴥᵒᶅ";
 		}
 	}
-	sensor('Yahyaa');
+	if (!empty($_POST['nama']) && isset($_POST['nama'])){
+	sensor($_POST['nama']);
+	}
 ?>
+	</body>
+</html>
